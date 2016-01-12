35d18756c31ef6f5945c00324979189da01ac710
test02 <zijie.zhang@dbjtech.com>
2016-01-12 16:37:43 +0800

    修改成打印执行过程

--- testFibo.js
+++ testFibo.js
@@ -1,32 +1,59 @@
 var co = require('co')
 var Promise = require('promise')
 
-//co执行
-function fibo(n) {
+//打印过程
+function* fibo(n) {
 	if(n === 0) {
 		return 0;
 	}
 	if(n == 1 || n == 2) {
 		return 1;
 	}
-	return fibo(n-1) + fibo(n-2);
+	var z = (yield fibo(n-1)) + (yield fibo(n-2));
+	//var z = fibo(n-1) + fibo(n-2);
+	console.log(z);
+	yield sleep();
+	return z;
 }
-function* gen(num) {
-	for (var i = 0; i <= num; i++) {
-		var z = fibo(i);
-		yield show(z);
-	}
-}
-function show(val){
+
+function sleep(){
 	return new Promise(function(resolve) {
 		setTimeout(function(){
-         	console.log(val);
          	resolve();
     	},1000)
 	})
 }
-var g = gen(10)
-co(g)
+co(fibo(5)).then(function (){
+ 	console.log('Generator 函数执行完成');
+})
+
+
+//co执行
+// function fibo(n) {
+// 	if(n === 0) {
+// 		return 0;
+// 	}
+// 	if(n == 1 || n == 2) {
+// 		return 1;
+// 	}
+// 	return fibo(n-1) + fibo(n-2);
+// }
+// function* gen(num) {
+// 	for (var i = 0; i <= num; i++) {
+// 		var z = fibo(i);
+// 		yield show(z);
+// 	}
+// }
+// function show(val){
+// 	return new Promise(function(resolve) {
+// 		setTimeout(function(){
+//          	console.log(val);
+//          	resolve();
+//     	},1000)
+// 	})
+// }
+// var g = gen(10)
+// co(g)
 
 
 //手动执行next
